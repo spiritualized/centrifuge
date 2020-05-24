f54fdc84a3bb27e0564d063327fcc17ba03ac6c5
spiritualized <contact@spiritualized.io>
2020-05-23 21:29:12 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+    name='centrifuge-manager',
+    scripts=['centrifuge.py'],
+    version='1.0.0',
+    description="""Music release management application, which provides validation, repair, and standardization. 
+    Useful for managing large libraries.""",
+    url='https://github.com/spiritualized/centrifuge',
+    download_url='https://github.com/spiritualized/centrifuge/archive/v1.0.0.tar.gz',
+    keywords=['audio', 'library', 'management', 'metadata', 'validation', 'mp3', 'flac', 'python'],
+    install_requires=[
+        'bitstring==3.1.6',
+        'cleartag>=1.1.0',
+        'colored==1.4.2',
+        'lastfmcache>=1.2.3',
+        'metafix>=1.1.0',
+        'mutagen==1.44.0',
+        'ordered-set==3.1.1'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
