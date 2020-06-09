6090506d7ce02dade16c68377cbeaceb0a746480
spiritualized <contact@spiritualized.io>
2020-06-08 21:01:08 -0400

    Remove directory tree scanner

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ setup(
     name='centrifuge-manager',
     scripts=['centrifuge', 'centrifuge.bat'],
     packages=['.'],
-    version='1.0.0',
+    version='1.0.1',
     description="""Music release management application, which provides metadata validation, repair, and 
     standardization. Useful for managing large libraries.""",
     url='https://github.com/spiritualized/centrifuge',
-    download_url='https://github.com/spiritualized/centrifuge/archive/v1.0.0.tar.gz',
+    download_url='https://github.com/spiritualized/centrifuge/archive/v1.0.1.tar.gz',
     keywords=['audio', 'library', 'management', 'metadata', 'validation', 'mp3', 'flac', 'python'],
     install_requires=[
         'bitstring==3.1.6',
-        'cleartag>=1.1.0',
+        'cleartag>=1.2.1',
         'colored==1.4.2',
         'lastfmcache>=1.2.3',
-        'metafix>=1.1.0',
+        'metafix>=1.2.1',
         'mutagen==1.44.0',
-        'ordered-set==3.1.1'
+        'ordered-set==3.1.1',
+        'release-dir-scanner>=1.0.0'
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
